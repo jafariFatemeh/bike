044cab9d4221ec2d95072342c2882f1bfebc400d
jafariFatemeh <fatemehjafari020@gmail.com>
2022-10-21 16:40:12 +0400

    ..

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -3,7 +3,18 @@ package com.globomatics.bike.models;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
     private String name;
     private String email;
     private String phone;
@@ -59,6 +70,12 @@ public class Bike {
     }
     public void setContact(boolean contact) {
         this.contact = contact;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
 
 
