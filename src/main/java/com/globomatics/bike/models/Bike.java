338874c6796f0f4847ed065460ca4b437b0bfaa4
jafariFatemeh <fatemehjafari020@gmail.com>
2022-10-25 16:42:36 +0400

    .

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -8,7 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Bike {
 
     @Id
@@ -21,6 +25,8 @@ public class Bike {
     private String model;
     private String serialNumber;
     private BigDecimal purchasePrice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
     private Date purhaseDate;
     private boolean contact;
     public String getName() {
