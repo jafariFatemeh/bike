8b480879dbbcecee98cd2dd382a98b727cbd669b
jafariFatemeh <fatemehjafari020@gmail.com>
2022-10-27 15:22:52 +0400

    .

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -9,10 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
-
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Bike {
 
     @Id
